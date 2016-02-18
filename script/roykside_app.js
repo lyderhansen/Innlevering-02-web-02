84976337d931b67c58eafecc12e3e6ba51d112a5
morkeseth <pamoerk@gmail.com>
2016-02-18 22:33:11 +0100

    fade in out på scroll + endret på nav

--- script/roykside_app.js
+++ script/roykside_app.js
@@ -1,57 +1,50 @@
-(function() {
+(function () {
     var ROYKSIDE_APP = {
         //HTML-objekter
         $sirkel1: null,
         $liste: null,
 
         //init-funksjoner
-        init: function() {
+        init: function () {
             var RA = ROYKSIDE_APP;
 
-            var setElements = function() {
+            var setElements = function () {
                 RA.$sirkel1 = $(".sirkel1");
                 RA.$liste = $(".arButton")
             }();
 
-            var setEvents = function() {
-<<<<<<< HEAD
+            var setEvents = function () {
 
 
                 RA.seEtterAr();
                 RA.leggTilId();
                 RA.endreInfoBoks();
-=======
                 RA.$sirkel1.click(RA.endreInfoBoks);
                 RA.$liste.click(RA.seEtterAr)
                 $(document).ready(RA.leggTilId);
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
 
             }();
 
         }, //--end init
 
         //Bruker endreInfoBoks funksjonen fra modulen
-        endreInfoBoks: function() {
+        endreInfoBoks: function () {
             ROYKSIDE_MODULE.endreInfoBoks();
-<<<<<<< HEAD
 
-
-=======
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
         }, //--end endreInfoBoks
 
         //Bruker leggTilId funksjonen fra modulen
-        leggTilId: function() {
+        leggTilId: function () {
             ROYKSIDE_MODULE.leggTilId();
         }, //--end leggTilId
 
-        seEtterAr: function(){
-            ROYKSIDE_MODULE.seEtterAr();
-        } //--end seEtterAr
+        seEtterAr: function () {
+                ROYKSIDE_MODULE.seEtterAr();
+            } //--end seEtterAr
 
     }; //--end SLIDESHOWAPP
 
-    $(function() {
+    $(function () {
         ROYKSIDE_APP.init();
     });
 
--- script/roykside_module.js
+++ script/roykside_module.js
@@ -1,65 +1,37 @@
-var ROYKSIDE_MODULE = (function() {
+var ROYKSIDE_MODULE = (function () {
     var artikkelJSONliste = {
         artikler: [{
-<<<<<<< HEAD
             "Arstall": "2011",
-=======
-            "Arstall": 2011,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "Fredag 22. juli 2011 - Terrordagen – Hendelsesforløp",
             "Tekst": "Kl. 15.13.23: Overvåkingskamera fanger opp at Anders Behring Breivik kjører bombebilen inn i Grubbegata.\n\nKl. 15.17.30: Iført en hjemmelaget politiuniform kommer Breivik seg uhindret avgårde fra Einar Gerhardsens plass og departementene, viftende med en pistol i høyre hånd.\n\nKl. 15.25.19: En bilbombe går av i Grubbegata i Regjeringskvartalet. Eksplosjonen har skjedd mellom høyblokken hvor Statsministerens kontor og Justisdepartementet holder til og R4, som huser Olje- og energidepartementet og Nærings- og handelsdepartementet.  ...",
             "Link": "http://bit.ly/Tv2TerrorMassakren"
         }, {
-<<<<<<< HEAD
             "Arstall": "2012",
-=======
-            "Arstall": 2012,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "Jakten på rasistvolden etter 22.juli",
             "Tekst": "Er det sant at innvandrere ble banket opp etter at bomben gikk av 22. juli? Landets 27 politidistrikter har søkt i loggene og ikke funnet en eneste hendelse\n\n22. juli 2011 var Houda Thaythay så tidlig ferdig på et datakurs i Oslo at hun rakk fredagsbønnen. Den norskmarokkanske kvinnen hadde satt seg i moskeen noen få hundre meter fra Regjeringskvartalet da hun hørte et kraftig smell. Bygningen ristet, husker hun, flere skrek. Imamen beroliget: \"Det var kanskje bare torden.\" Så, ny beskjed: \"Forlat lokalene!\"\n\nHvorfor? Hva skjer? De gikk usikre ut på gaten. Gasseksplosjon, sa noen. Bombe, sa andre. Uroen vokste da Houda og to damer gikk gjennom sentrum, mot bussen få minutter etter eksplosjonen. Utenfor 7 Eleven i Storgata sto flere samlet. Ifølge Thaythay pekte de på henne og venninnene i hijab, for så å rope: \"Det er Al-Qaida som har gjort det! Det er Al-Qaida!\"",
             "Link": "http://bit.ly/AftenJakten"
         }, {
-<<<<<<< HEAD
             "Arstall": "2013",
-=======
-            "Arstall": 2013,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "22. juli er blitt borte fra forsidene",
             "Tekst": "IDEER: Det kommer ikke lenger ut flere 22. juli-bøker i måneden. Men debatten er ikke død. På mandag kom Tom Egil Hverven og Sverre Mallings bok «Terrorens ansikt» ut — med Hvervens bearbeidede rettskommentarer, Mallings rettstegninger, og en samtale mellom de to om kunst og terror. I forrige uke vant Aage Storm Borchgrevink Kritikerprisen for årets beste sakprosabok, med «En norsk tragedie». Et halvt år etter utgivelsen av «Jeg er Prableen» balanserer Prableen Kaur for tiden jusstudier med plassen i bystyret for Oslo Ap, og øvrige verv i AUF og moderpartiet. \n\nVi samlet dem alle på en kafé på Youngstorget for å spørre: \n\n- Begynner den kollektive fortellingen om hvordan vi skal forstå 22. juli å feste seg?",
             "Link": "http://bit.ly/DB22juliBorte"
         }, {
-<<<<<<< HEAD
             "Arstall": "2014",
-=======
-            "Arstall": 2014,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "22. juli-terroren - et tilbakeblikk",
             "Tekst": "Hva skjedde 22. juli 2011 under terrorangrepene mot regjeringskvartalet i Oslo og AUF-leiren på Utøya i Tyrifjorden?\nKlokka 15.25 fredag 22. juli 2011 eksploderte en 950 kilos bombe i regjeringskvartalet i Oslo. Det var starten på terrorangrepene som skulle koste i alt 77 mennesker livet. Åtte mennesker ble drept i bombekeplosjonen og ytterligere 69 personer - 33 av dem under 18 år - ble drept under massakren på Utøya.\n\nRundt 90 personer ble såret i terrorangrepene.\n\nGjerningsmannen Anders Behring Breivik ble i august året etter dømt til forvaring i 21 år for massemord, for å ha forårsaket en dødelig bombeeksplosjon og for terrorisme.",
             "Link": "http://bit.ly/Nettavisen22Juli"
         }, {
-<<<<<<< HEAD
             "Arstall": "2015",
-=======
-            "Arstall": 2015,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "Ordførerkandidat: - 22. juli skjedde med Guds tillatelse",
             "Tekst": "Dagbladet: - Det er mange vettuge tanker i boka. Jeg stiller meg bak budskapet at Gud tillot 22. juli-angrepene på grunn av Arbeiderpartiet og AUFs Israel-motstand, sier Kjartan Mogen (51) til Dagbladet.\n\n«22. juli-profetien» har skapt betydelig kontrovers de siste dagene. Boka, skrevet av norskamerikanske Jeremy Hoff (34), har som hovedbudskap at terrorangrepene 22. juli 2011 skjedde fordi Norge, og særlig Arbeiderpartiet og AUF, svikter i sin støtte til Israel.\n\nHoffs bok har fått betydelig positiv omtale, blant annet på den kristne TV-kanalen TV Visjon. I tillegg låner kanalens pastor Jan Hanevold ut navnet sitt til markedsføringen av boka, sammen med en rekke andre personligheter fra Kristen-Norge, deriblant eks-KrF-politiker Anita Apelthun Sæle.",
             "Link": "http://bit.ly/DBordfoerer"
         }, {
-<<<<<<< HEAD
             "Arstall": "2015",
-=======
-            "Arstall": 2015,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "Har fjernet Breiviks utstyrskoffert fra 22.juli-senteret",
             "Tekst": "Gjerningsmannens utstyrskoffert ble tatt bort fra utstillingen, fordi den ikke hadde nok fortellerkraft.\n\nOnsdag åpner det nye informasjonssenteret om 22. juli-terroren i Høyblokkas første etasje.\n\nRestene av bombebilen som den terrordømte massedrapsmannen Anders Behring Breivik brukte for å sprenge Høyblokka for fire år siden, er en del av utstillingen.\n\nOgså Breiviks falske ID-kort og politiemblemer er blant de øvrige gjenstandene som folk kan få se i utstillingen som offisielt åpner 22. juli.",
             "Link": "http://bit.ly/VGutstyrskoffert"
         }, {
-<<<<<<< HEAD
             "Arstall": "2015",
-=======
-            "Arstall": 2015,
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
             "Tittel": "Halvparten av 22. juli-tiltakene ble ikke innført",
             "Tekst": "Det skulle være politiets store omstillingsprogram etter all kritikken i kjølvannet av 22. juli. Men til slutt stoppet Politidirektoratet å telle tiltakene og la ned programmet.\nOver fire år etter terrorangrepet mot Utøya og regjeringskvartalet, setter 22. juli fortsatt sitt tydelige preg på politiet.\n\nKritikken fra 22. juli-kommisjonen sparket i gang en storstilt omstilling, og en endeløs rekke tiltak for å gjøre ordensmakten bedre.",
             "Link": "http://bit.ly/BTHalvpartenAvTiltakene"
@@ -67,71 +39,59 @@ var ROYKSIDE_MODULE = (function() {
     };
 
     //Legger til id til hver sirkel så de henter ut riktig tekst fra JSON liste
-    var leggTilId = function() {
+    var leggTilId = function () {
         var antallPoster = ("circle").length;
         for (var i = 0; i < antallPoster; i++) {
             $("circle")
-                .attr("id", function(i) {
+                .attr("id", function (i) {
                     return (i)
                 })
         }
     }; //--End leggTilId
 
     //Endrer teksten i infoboksen
-    var endreInfoBoks = function() {
+    var endreInfoBoks = function () {
         var klikketId = "";
         var allDots = $("svg.dots > *");
-        $.each(artikkelJSONliste.artikler, function() {
-            $(".sirkel1").on("click", function() {
+        $.each(artikkelJSONliste.artikler, function () {
+            $(".sirkel1").on("click", function () {
                 allDots.removeClass("on");
                 $(this).addClass("on");
-<<<<<<< HEAD
                 klikketId = $(this).attr("id");
-=======
-                klikketId = $(this).attr("id")
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
                 $("#tekstboks").animate({
                         opacity: 1
-                    }, 1000, function() {}),
+                    }, 1000, function () {}),
                     $("#tekstBoksHeader").html(artikkelJSONliste.artikler[klikketId].Tittel + ", " + artikkelJSONliste.artikler[klikketId].Arstall),
                     $("#tekstBoksTxt").html(artikkelJSONliste.artikler[klikketId].Tekst),
                     $("#linkInTekstBoks").attr('href', artikkelJSONliste.artikler[klikketId].Link),
-<<<<<<< HEAD
-=======
-                    //$("#linkInTekstBoks").html(artikkelJSONliste[klikketId].Link),
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
                     $("#tekstBoksLink").css({
                         "color": "white",
                     })
             })
-<<<<<<< HEAD
 
-=======
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
         })
     }; //--End endreInfoBoks
 
     //Ser etter artikler som er laget i det året som står
-<<<<<<< HEAD
-    var seEtterAr = function() {
+    var seEtterAr = function () {
         var klikketAr = "";
         var $alleSirkler = $(".sirkel1");
-        $.each(artikkelJSONliste.artikler, function(index, value) {
-            $(".arButton").on("click", function() {
+        $.each(artikkelJSONliste.artikler, function (index, value) {
+            $(".arButton").on("click", function () {
                 klikketAr = $(this).attr("id");
-                if(klikketAr == "alle"){
+                if (klikketAr == "alle") {
                     $alleSirkler.css("fill", "rgb(255,255,255)");
                 }
                 if (klikketAr == value.Arstall) {
-                    if(value.Arstall == 2011){
+                    if (value.Arstall == 2011) {
                         $("#" + index).css("fill", "rgb(237,178,0)");
-                    } else if (value.Arstall == 2012){
+                    } else if (value.Arstall == 2012) {
                         $("#" + index).css("fill", "rgb(163,0,0)");
-                    } else if (value.Arstall == 2013){
+                    } else if (value.Arstall == 2013) {
                         $("#" + index).css("fill", "rgb(201,201,201)");
-                    } else if (value.Arstall == 2014){
+                    } else if (value.Arstall == 2014) {
                         $("#" + index).css("fill", "rgb(121,94,57)");
-                    } else if (value.Arstall == 2015){
+                    } else if (value.Arstall == 2015) {
                         $("#" + index).css("fill", "rgb(39,36,36)");
                     }
                 }
@@ -140,19 +100,7 @@ var ROYKSIDE_MODULE = (function() {
         })
 
     }; //--end seEtterAr
-=======
-    var seEtterAr = function(){
-        var klikketAr = $(this).attr("id");
-        $.each(artikkelJSONliste.artikler, funtion() {
-            alert(klikketAr)
-            if(klikketAr == this.Arstall){
-                //$(this).css("fill","#334400");
 
-            }
-        })
-
-    };//--end seEtterAr
->>>>>>> b21eb2666aa302032e542ab6238cf057e9fea364
 
     //returnerer funksjonene så de kan brukes i appen
     return {
